a0ce935f610ca3502577a68ddf286bd4974b0210
zak258 <zakaria.k.hamdan@gmail.com>
2020-08-24 12:59:35 +0200

    boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment, useState } from "react";
+
+const App = () => {
+  //   const [,] = useState();
+  return (
+    <Fragment>
+      <h1>hey</h1>
+    </Fragment>
+  );
+};
+
+export default App;
