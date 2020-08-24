88d5736f22ba96f83cb9919c764f09c39a4267bb
zak258 <zakaria.k.hamdan@gmail.com>
2020-08-24 17:01:43 +0200

    homepage circles

--- src/components/circles.jsx
+++ src/components/circles.jsx
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Circles = () => {
+  let today = new Date(),
+    time = today.getHours() + ":" + today.getMinutes();
+  let myDate = new Date();
+  let hrs = myDate.getHours();
+  let greet;
+  if (hrs < 12) greet = "Good Morning";
+  else if (hrs >= 12 && hrs < 17) greet = "Good Afternoon";
+  else if (hrs >= 17 && hrs <= 24) greet = "Good Evening";
+  const [greeting, changeGreat] = useState(greet);
+  const [date, change] = useState(time);
+
   return (
     <div className="circles">
       <div className="greeting">
-        <label id="lblGreetings"></label>
+        <div id="lblGreetings">{greeting}</div>
       </div>
       <div className="row big-circle">
-        <div className="small-circle">
-          <form name="clockform">
-            <input type="text" name="clock" className="time-input" />
-          </form>
-        </div>
+        <div className="small-circle">{date}</div>
       </div>
     </div>
   );
 };
-
 export default Circles;
