ca4e4921eaf99b8b380ee499c8fcf1786ada95d3
zak258 <zakaria.k.hamdan@gmail.com>
2020-09-01 09:35:15 +0200

    minor

---
+++ build/precache-manifest.ad59754fb56cfe2abecebfb71e22218b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3965acd12839ec60d1e77988712f06e8",
+    "url": "/portfolio-React/build/index.html"
+  },
+  {
+    "revision": "8ebd90e9b1016be143b1",
+    "url": "/portfolio-React/build/static/css/main.2c08936a.chunk.css"
+  },
+  {
+    "revision": "55a4df7d09bc157fd262",
+    "url": "/portfolio-React/build/static/js/2.01ec1337.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/portfolio-React/build/static/js/2.01ec1337.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ebd90e9b1016be143b1",
+    "url": "/portfolio-React/build/static/js/main.7170baec.chunk.js"
+  },
+  {
+    "revision": "468299fed93438d6653e",
+    "url": "/portfolio-React/build/static/js/runtime-main.c6920ccb.js"
+  },
+  {
+    "revision": "608cf3312177e8619754f89b585c243e",
+    "url": "/portfolio-React/build/static/media/IMG_2828.608cf331.JPG"
+  },
+  {
+    "revision": "f49d6a063045e4f29ab59da7186baa93",
+    "url": "/portfolio-React/build/static/media/IMG_2874.f49d6a06.JPG"
+  },
+  {
+    "revision": "a5af0334d29a2624a5aed07d8ed9a255",
+    "url": "/portfolio-React/build/static/media/IMG_2879.a5af0334.JPG"
+  },
+  {
+    "revision": "1a30759eeac2defb615ab294f039bfc9",
+    "url": "/portfolio-React/build/static/media/css-drows.1a30759e.png"
+  },
+  {
+    "revision": "1cc2fffa5eee0c91e5d68eaac4783232",
+    "url": "/portfolio-React/build/static/media/hub.1cc2fffa.png"
+  },
+  {
+    "revision": "3af0bc2d343e5d8e7b5b8edae1a3c3e7",
+    "url": "/portfolio-React/build/static/media/paper-1074131_1920.3af0bc2d.jpg"
+  },
+  {
+    "revision": "7b8b784f87b98382b0f2d52392054ca2",
+    "url": "/portfolio-React/build/static/media/profile.7b8b784f.jpg"
+  },
+  {
+    "revision": "f815cc37e6399a1647bd122d3b2814cd",
+    "url": "/portfolio-React/build/static/media/tear-drops.f815cc37.jpg"
+  },
+  {
+    "revision": "4eb76898331cad7bf45fcbffe2f198bc",
+    "url": "/portfolio-React/build/static/media/wall.4eb76898.jpg"
+  },
+  {
+    "revision": "7cb59cdd9f4114fb3b7a9cf979a1dece",
+    "url": "/portfolio-React/build/static/media/wood.7cb59cdd.jpg"
+  }
+]);
