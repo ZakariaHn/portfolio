ab50ec4e958a20607e276c6ad53fd00ca202b96e
zak258 <zakaria.k.hamdan@gmail.com>
2020-09-29 13:23:57 +0200

    Updates

---
+++ precache-manifest.ed7a3ff4c48f5e97a4fe35974270b6ab.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df70217b4704445c343d02d98b10c86",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "35376d42ca5fecae4645",
+    "url": "/portfolio/static/css/main.b42e9f35.chunk.css"
+  },
+  {
+    "revision": "98e740a71184ca1bcabd",
+    "url": "/portfolio/static/js/2.79ad8241.chunk.js"
+  },
+  {
+    "revision": "4e95a99521d36b6bc5e926f9ef6855ee",
+    "url": "/portfolio/static/js/2.79ad8241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35376d42ca5fecae4645",
+    "url": "/portfolio/static/js/main.ac9b1865.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "bb89ec3cb20820ca7de07c79f8a1e90c",
+    "url": "/portfolio/static/media/css-drows.bb89ec3c.png"
+  },
+  {
+    "revision": "3af0bc2d343e5d8e7b5b8edae1a3c3e7",
+    "url": "/portfolio/static/media/paper-1074131_1920.3af0bc2d.jpg"
+  },
+  {
+    "revision": "7b8b784f87b98382b0f2d52392054ca2",
+    "url": "/portfolio/static/media/profile.7b8b784f.jpg"
+  },
+  {
+    "revision": "11c708230d09098187c0e61434bf563c",
+    "url": "/portfolio/static/media/rosetta.11c70823.png"
+  },
+  {
+    "revision": "36c9736e1aaf7b7e3ad7f54ba1f06bbc",
+    "url": "/portfolio/static/media/synth.36c9736e.png"
+  },
+  {
+    "revision": "f815cc37e6399a1647bd122d3b2814cd",
+    "url": "/portfolio/static/media/tear-drops.f815cc37.jpg"
+  },
+  {
+    "revision": "4eb76898331cad7bf45fcbffe2f198bc",
+    "url": "/portfolio/static/media/wall.4eb76898.jpg"
+  },
+  {
+    "revision": "7cb59cdd9f4114fb3b7a9cf979a1dece",
+    "url": "/portfolio/static/media/wood.7cb59cdd.jpg"
+  }
+]);
