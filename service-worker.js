56d8ae1a1c429cde7bcea037504f26670e90534d
zak258 <zakaria.k.hamdan@gmail.com>
2020-10-02 00:38:55 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e42bd2b50780d271fdb190afafe2b2b6.js"
+  "/portfolio/precache-manifest.9b656ce5c736d4ff90e1c15ec70d6a38.js"
 );
 
 self.addEventListener('message', (event) => {
